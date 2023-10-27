56fd3ae8c0c17e4327bbd0414e6f100699acab3b
MilPredo <milferdsonsalanga@gmail.com>
2023-10-27 22:54:54 +0800

    added client-side inventory query

---
+++ src/api/inventory.tsx
@@ -0,0 +1,28 @@
+const getInventory = async (page: number) => {
+  // Define the base URL of the API
+  const baseUrl = "http://127.0.0.1:1338/inventory"; // Replace with your API URL
+
+  // Create a URLSearchParams object to manage query parameters
+  const queryParams = new URLSearchParams();
+  queryParams.append("page", `${page}`); // Replace '1' with the desired page number
+
+  // Combine the base URL with the query parameters
+  const apiUrl = `${baseUrl}?${queryParams.toString()}`;
+
+  // Create the fetch request
+  try {
+    let response = await fetch(apiUrl, {
+      method: "GET",
+      headers: {
+        "Content-Type": "application/json",
+        // You can add any other headers if necessary
+      },
+    });
+    console.log(await response.json());
+  } catch (error) {
+    console.error("Fetch error:", error);
+  }
+};
+
+
+export default getInventory
