0b5924782cd870427f08dd785c4aa15c1035145f
milpredo <milferdsonsalanga@gmail.com>
2023-11-25 20:26:04 +0800

    change npm run dev

--- run.bat
+++ run.bat
@@ -1,10 +1,18 @@
 @echo off
+git init
+git remote set-url origin https://github.com/MilPredo/XenoCore.git
+taskkill /im node.exe /f
 echo Fetching latest dev branch...
 git checkout develop
 git pull origin develop
 echo Done!...
 echo Starting InSys!...
-start cmd /k "echo Starting Front-End Application... && npm i && npm run dev"
-start cmd /k "echo Starting Back-End Application... && cd server && npm i && npm run dev"
+npm i
+start cmd /k "echo Starting Front-End Application... && npm run dev && exit"
+cd server
+npm i
+start cmd /k "echo Starting Back-End Application... npm run dev && exit"
 echo running...
+start "" "http://127.0.0.1:5173"
+taskkill /im node.exe /f
 pause
